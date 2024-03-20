065b58fa5d471cdedf5a5075f0d287ab4dcf21b9
aadit engineer <aaditengineer@gmail.com>
2024-03-19 23:42:25 -0400

    MVP minus font

--- script-practice/App.js
+++ script-practice/App.js
@@ -1,12 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import OpenAI from 'openai';
+import { Feather } from '@expo/vector-icons';
+
+// const apiKey = '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';
 
 export default function App() {
+  const [generatedString, setGeneratedString] = useState('a');
+  const [text, onChangeText] = useState('');
+
+  //const openai = new OpenAI({apiKey: apiKey, organization: 'org-FUgdswP2oGJFFyVarog8XpoQ', dangerouslyAllowBrowser: true, });
+
+  const generateString = async () => {
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [{role: "system", content: "Generate a simple sentence that would be used to teach reading English."}],
+        model: "gpt-3.5-turbo",
+      });
+      console.log(completion.choices[0])
+      
+      //setGeneratedString(completion.choices[0].message.content);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const fakeGenerateString = async () => {
+    setGeneratedString("The cat ran fast.");
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <h1>Script Practice</h1>
+      <Button
+        title="Generate sentence"
+        color="#f194ff"
+        onPress={fakeGenerateString}
+      />
+      {generatedString !== 'a' && (
+        <>
+        <Text>Sentence is Below...</Text>
+        <Text>{generatedString}</Text>
+        <TextInput
+          style={(text != generatedString) ? styles.redInput : styles.successInput}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={"Write the sentence in normal English script."}
+        />
+        </>
+      )}
+      {(generatedString.trim() == text) && (
+        <Feather name="check-circle" size={24} color="#39FF14" />
+      )}
     </View>
   );
 }
@@ -17,5 +62,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  font: {
+    //fontFamily: '',
+  },
+  redInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#f194ff",
+  },
+  successInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#84a98c",
   },
 });
