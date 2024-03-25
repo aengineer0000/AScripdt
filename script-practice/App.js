a61d7ea00fa64d2c54502f0ef02004f91975eccb
aadit engineer <aaditengineer@gmail.com>
2024-03-25 15:13:26 -0400

    fonts uploaded

--- script-practice/App.js
+++ script-practice/App.js
@@ -1,57 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import OpenAI from 'openai';
 import { Feather } from '@expo/vector-icons';
 
- const apiKey = process.env.OPENAI_API_KEY;
+const apiKey = process.env.OPENAI_API_KEY;
 
 export default function App() {
   const [generatedString, setGeneratedString] = useState('a');
   const [text, onChangeText] = useState('');
+  const [fontsLoaded, fontError] = Font.useFonts({
+    "Ascridpt-Regular": require("./assets/fonts/Ascripdt-Regular.ttf"),
+  });
 
-  //const openai = new OpenAI({apiKey: apiKey, organization: 'org-FUgdswP2oGJFFyVarog8XpoQ', dangerouslyAllowBrowser: true, });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   const generateString = async () => {
     try {
       const completion = await openai.chat.completions.create({
-        messages: [{role: "system", content: "Generate a simple sentence that would be used to teach reading English."}],
+        messages: [{ role: "system", content: "Generate a simple sentence that would be used to teach reading English."}],
         model: "gpt-3.5-turbo",
       });
-      console.log(completion.choices[0])
-      
+      console.log(completion.choices[0]);
+
       //setGeneratedString(completion.choices[0].message.content);
     } catch (error) {
       console.error('Error:', error);
     }
   };
+
   const fakeGenerateString = async () => {
     setGeneratedString("The cat ran fast.");
-  }
-
+  };
 
   return (
     <View style={styles.container}>
-      <h1>Script Practice</h1>
-      <Button
-        title="Generate sentence"
-        color="#f194ff"
-        onPress={fakeGenerateString}
-      />
-      {generatedString !== 'a' && (
-        <>
-        <Text>Sentence is Below...</Text>
-        <Text>{generatedString}</Text>
-        <TextInput
-          style={(text != generatedString) ? styles.redInput : styles.successInput}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder={"Write the sentence in normal English script."}
-        />
-        </>
-      )}
-      {(generatedString.trim() == text) && (
-        <Feather name="check-circle" size={24} color="#39FF14" />
-      )}
+          <h1>Script Practice</h1>
+          <Button
+            title="Generate sentence"
+            color="#f194ff"
+            onPress={fakeGenerateString}
+          />
+          {generatedString !== 'a' && (
+            <>
+              <Text>Sentence is Below...</Text>
+              {fontsLoaded ? (
+              <Text style={styles.font}>{generatedString}</Text>
+              ) : (
+                <Text>Loading fonts...</Text>  // Display loading message while fonts load
+              )}
+              <TextInput
+                style={(text != generatedString) ? styles.redInput : styles.successInput}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder={"Write the sentence in normal English script."}
+              />
+            </>
+          )}
+          {(generatedString.trim() == text) && (
+            <Feather name="check-circle" size={24} color="#39FF14" />
+          )}
     </View>
   );
 }
@@ -62,9 +79,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  font: {
-    //fontFamily: '',
   },
   redInput: {
     height: 40,
@@ -80,4 +94,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderColor: "#84a98c",
   },
+  font: {
+    fontFamily: "Ascridpt-Regular",
+    fontSize: 60,
+  }
 });
